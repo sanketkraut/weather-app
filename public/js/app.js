8136d93a61476e5eb7a7e70b6970deac586cd1c4
Sanket Raut <rautsk22@gmail.com>
2021-05-22 17:33:33 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
     const address = search.value
     message1.textContent = 'Loading...'
     message2.textContent = ''
-    fetch('http://localhost:3000/weather?address='+address).then((response) => {
+    fetch('/weather?address='+address).then((response) => {
     response.json().then(({error, location, forecast} ={}) => {
         if(error){
             return message1.textContent = error
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //set paths
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -75,6 +76,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
